eeed978e7acc7e429a542873d2c623b5edf8dcff
Hemant Paneru <hpaneru.hp@gmail.com>
2022-03-30 20:58:28 +0545

    minnor changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey, Whats up? Sexy Peoples!
+          Hey, Whats up? Sexy Peoples! ~ HP
           {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         {/* <a
